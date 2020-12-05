42d4c2b87826fd29f1baa28fba42d6e2f9bb4d88
MASA20201203 <koedame032@gmail.com>
2020-12-05 15:34:11 +0900

    複数の条件式

--- javascript1.js
+++ javascript1.js
@@ -1,12 +1,7 @@
-const age = 17;
+const age = 24;
 
-// ageの値が10以上20未満のとき、「私は20歳未満ですが、10歳以上です」と出力してください
-if (age >= 20) {
-  console.log("私は20歳以上です");
-} else if (age >= 10) {
-  console.log("私は20歳未満ですが、10歳以上です");
-}
+// 指定された条件のif文を作成してください
 
- else {
-  console.log("私は10歳未満です");
+if (age >= 20 && age < 30) {
+  console.log("私は20代です");
 }
