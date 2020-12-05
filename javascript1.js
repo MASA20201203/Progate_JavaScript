5434f291bd8f6017201ede728521016b1179f285
MASA20201203 <koedame032@gmail.com>
2020-12-05 15:38:42 +0900

    switch文①

--- javascript1.js
+++ javascript1.js
@@ -1,7 +1,17 @@
-const age = 24;
+const rank = 2;
 
-// 指定された条件のif文を作成してください
+switch (rank) {
+  case 1:
+    console.log("金メダルです！");
+    break;
 
-if (age >= 20 && age < 30) {
-  console.log("私は20代です");
+  // rankの値が2のcaseを追加してください
+  case 2:
+    console.log("銀メダルです！");
+    break;
+  // rankの値が3のcaseを追加してください
+  case 3:
+    console.log("銅メダルです！");
+    break;
+  
 }
