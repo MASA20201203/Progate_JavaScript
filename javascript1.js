08d17bf620c98c0aa28dcf3c341261025b4ad612
MASA20201203 <koedame032@gmail.com>
2020-12-05 15:11:13 +0900

    テンプレートリテラル

--- javascript1.js
+++ javascript1.js
@@ -2,7 +2,7 @@ const name = "にんじゃわんこ";
 const age = 14;
 
 // 「ぼくの名前は〇〇です」とコンソールに出力してください
-console.log(`ぼくのなまえは${name}です`);
+console.log(`ぼくの名前は${name}です`);
 
 // 「今は〇〇歳です」と出力してください
 console.log(`今は${age}歳です`);
