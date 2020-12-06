83dcc5ddb663b2ad726a320346d7b93283c8e549
MASA20201203 <koedame032@gmail.com>
2020-12-06 09:14:26 +0900

    配列と繰り返し①

--- javascript2.js
+++ javascript2.js
@@ -1,7 +1,5 @@
 const animals = ["dog", "cat", "sheep"];
 
-// 配列の1つ目の要素を出力してください
-console.log(animals[0]);
-
-// 配列の3つ目の要素を出力してください
-console.log(animals[2]);
+for (let i = 0; i < 3; i ++) {
+  console.log(animals[i]);
+}
