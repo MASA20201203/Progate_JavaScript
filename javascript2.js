97ee9a2633b6ee8355038c10983c42468691c6f6
MASA20201203 <koedame032@gmail.com>
2020-12-06 09:07:55 +0900

    配列とは

--- javascript2.js
+++ javascript2.js
@@ -1,7 +1,2 @@
-for (let number = 1; number <= 100; number++) {
-  if (number % 3 === 0) {
-    console.log("3の倍数です");
-  } else {
-    console.log(number);
-  }
-}
+const animals = ["dog", "cat", "sheep"];
+console.log(animals);
