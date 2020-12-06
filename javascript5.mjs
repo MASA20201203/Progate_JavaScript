ce6c52010cd3e699ade8b4aad709ca284470d011
MASA20201203 <koedame032@gmail.com>
2020-12-06 15:09:35 +0900

    名前付きエクスポート

--- javascript5.mjs
+++ javascript5.mjs
@@ -1,3 +1,6 @@
-import dog from "./javascript5_dogData.mjs";
+import { dog1, dog2 } from "./javascript5_dogData.mjs";
 
-dog.info();
+console.log("---------");
+dog1.info();
+console.log("---------");
+dog2.info();
--- javascript5_dogData.mjs
+++ javascript5_dogData.mjs
@@ -1,5 +1,8 @@
 import Dog from "./javascript5_dog.mjs";
 
-const dog = new Dog("レオ", 4, "チワワ");
+// 定数dog1, dog2を確認してください
+const dog1 = new Dog("レオ", 4, "チワワ");
+const dog2 = new Dog("ベン", 2, "プードル");
 
-export default dog;
+// 以下を書き換えて、定数dog1, dog2をエクスポートしてください
+export { dog1, dog2 };
