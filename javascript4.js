005eb825f5497b504c86c721427c122f2e8e04d4
MASA20201203 <koedame032@gmail.com>
2020-12-06 14:15:05 +0900

    オーバーライド②

--- javascript4.js
+++ javascript4.js
@@ -16,19 +16,26 @@ class Animal {
 }
 
 class Dog extends Animal {
-  // infoメソッドを追加してください
+  // constructorを追加してください
+  constructor(name, age, breed) {
+    super(name, age);
+    this.breed = breed;
+  }
   info() {
     this.greet();
     console.log(`名前は${this.name}です`);
+    // 「犬種は〇〇です」と出力してください
+    console.log(`犬種は${this.breed}です`);
     console.log(`${this.age}歳です`);
-    
     const humanAge = this.getHumanAge();
     console.log(`人間年齢で${humanAge}歳です`);
   }
+
   getHumanAge() {
     return this.age * 7;
   }
 }
 
-const dog = new Dog("レオ", 4);
+// 3つ目の引数に「"チワワ"」を渡してください
+const dog = new Dog("レオ", 4, "チワワ");
 dog.info();
