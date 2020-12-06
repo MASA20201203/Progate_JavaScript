8a501bebdbcac4011aef050a5eee4d3bdb4201a8
MASA20201203 <koedame032@gmail.com>
2020-12-06 10:07:40 +0900

    引数とは

--- javascript3.js
+++ javascript3.js
@@ -1,5 +1,8 @@
-const greet = () => {
-  console.log("こんにちは！");
-}
+// 関数の引数にnameを追加してください
+const greet = (name) => {
+  // 「こんにちは、〇〇さん」となるように出力してください
+  console.log("こんにちは、" + name + "さん");
+};
 
-greet();
+// greetの引数に「ひつじ仙人」を渡して呼び出してください
+greet("ひつじ仙人");
