3fbef17a59a91ace251b6bf4bfc11b76884f402f
MASA20201203 <koedame032@gmail.com>
2020-12-06 09:19:05 +0900

    オブジェクトとは

--- javascript2.js
+++ javascript2.js
@@ -1,9 +1,2 @@
-const animals = ["dog", "cat", "sheep", "rabbit", "monkey", "tiger", "bear", "elephant"];
-
-// lengthを用いて配列の要素の数を出力してください
-console.log(animals.length);
-
-// lengthを用いて条件式を書き換えてください
-for (let i = 0; i < animals.length; i++) {
-  console.log(animals[i]);
-}
+const character = {name:"にんじゃわんこ", age:14};
+console.log(character);
