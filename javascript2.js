aa52a321570ff8f7b82394bf2474434558dcb77c
MASA20201203 <koedame032@gmail.com>
2020-12-06 09:16:26 +0900

    配列と繰り返し②

--- javascript2.js
+++ javascript2.js
@@ -1,5 +1,9 @@
-const animals = ["dog", "cat", "sheep"];
+const animals = ["dog", "cat", "sheep", "rabbit", "monkey", "tiger", "bear", "elephant"];
 
-for (let i = 0; i < 3; i ++) {
+// lengthを用いて配列の要素の数を出力してください
+console.log(animals.length);
+
+// lengthを用いて条件式を書き換えてください
+for (let i = 0; i < animals.length; i++) {
   console.log(animals[i]);
 }
