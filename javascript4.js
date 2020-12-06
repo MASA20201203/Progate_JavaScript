1eba8725a053e179358d5f8ed9a374cb3061d1eb
MASA20201203 <koedame032@gmail.com>
2020-12-06 14:06:45 +0900

    オーバーライド①

--- javascript4.js
+++ javascript4.js
@@ -16,7 +16,15 @@ class Animal {
 }
 
 class Dog extends Animal {
-  // getHumanAgeメソッドを追加してください
+  // infoメソッドを追加してください
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+    
+    const humanAge = this.getHumanAge();
+    console.log(`人間年齢で${humanAge}歳です`);
+  }
   getHumanAge() {
     return this.age * 7;
   }
@@ -24,8 +32,3 @@ class Dog extends Animal {
 
 const dog = new Dog("レオ", 4);
 dog.info();
-
-// 定数humanAgeを定義し、定数dogに対してgetHumanAgeメソッドを呼び出した値を代入してください
-const humanAge = dog.getHumanAge();
-// 「人間年齢で〇〇歳です」と出力してください
-console.log(`人間年齢で${humanAge}歳です`);
